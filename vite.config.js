590fe6c22433829fb19ffa2885c6382c86d9c77e
Chathuranga Niroshana <134888303+Chathuranga-Niroshana@users.noreply.github.com>
2025-07-15 11:21:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
 export default defineConfig(({ mode }) => ({
   server: {
     host: false,
     port: 8083,
-  },
-   proxy: {
+    proxy: {
       // Forward all requests starting with /api to your backend
       '/api': {
         target: 'http://62.171.169.191:5000',
@@ -16,8 +14,8 @@ export default defineConfig(({ mode }) => ({
         secure: false, // Allow self-signed or insecure certificates (if HTTPS is used)
         rewrite: (path) => path.replace(/^\/api/, '') // optional: strip /api from the URL
       }
-   }
-    },
+    }
+  },
   plugins: [
     react(),
     mode === 'development'
