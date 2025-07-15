547c488480b9a768a9e3dde188c146a6d5a6f7aa
Chathuranga Niroshana <134888303+Chathuranga-Niroshana@users.noreply.github.com>
2025-07-15 11:18:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,15 @@ export default defineConfig(({ mode }) => ({
     host: false,
     port: 8083,
   },
+  proxy: {
+      // Forward all requests starting with /api to your backend
+      '/api': {
+        target: 'http://62.171.169.191:5000',
+        changeOrigin: true,
+        secure: false, // Allow self-signed or insecure certificates (if HTTPS is used)
+        rewrite: (path) => path.replace(/^\/api/, '') // optional: strip /api from the URL
+      }
+    }
   plugins: [
     react(),
     mode === 'development'
